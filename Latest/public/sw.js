1b902383bc71285de6250a112333df8f822941c3
Apoorv Tomar <apoorvtomar2222@github.com>
2020-09-06 20:22:51 +0530

    add build code.

--- Latest/public/sw.js
+++ Latest/public/sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-10741ff208e4a42a5bc2.js"
+    "url": "webpack-runtime-b335a6fdbd5daead0fec.js"
   },
   {
     "url": "framework-fd2b85530eec85104a39.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b93cf531b8370824d2d9ac6155ce738e"
+    "revision": "544ec72212cfd21b312e04160af28837"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
